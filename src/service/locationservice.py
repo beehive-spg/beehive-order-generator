93c3340daa8ed8e25bcc39408480c8918997ed52
Dschonas <jonasneumair@gmail.com>
2018-03-07 21:01:35 +0100

    Fix error that depended on a running db

--- src/service/locationservice.py
+++ src/service/locationservice.py
@@ -3,13 +3,10 @@ from geopy.distance import vincenty
 from data import rest
 from service import domainservice
 
-#MAXDISTANCE = ( (rest.get_drone_types_json()[0]['dronetype/range'] / 1000) / 2) *0.92
-MAXDISTANCE = 2500 * 0.92
-
 def is_reachable(lon, lat):
     all_hives = domainservice.get_all_hives()
     for hive in all_hives:
-        if (is_in_range((lon, lat), (hive.xcoord, hive.ycoord), MAXDISTANCE)):
+        if (is_in_range((lon, lat), (hive.xcoord, hive.ycoord), get_maxdistance)):
             return True
     return False
 
@@ -18,6 +15,9 @@ def is_in_range(a, b, maxdistance):
 
 def get_distance(a, b):
     return vincenty(a, b).km
+
+def get_maxdistance():
+    return ( (rest.get_drone_types_json()[0]['dronetype/range'] / 1000) / 2) *0.92
 
 def get_upper_x(all_hives):
     x = -1
